99af1d78e91d19bc76e0ea6816e18a8e5c4b3c02
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2024-08-27 10:20:35 -0400

    Servidor express de produtos.

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,5 +1,16 @@
-
+import Express from "express";
 import ListaProdutos from "./lista-produtos";
 
-const listaProdutos = new ListaProdutos()
-listaProdutos.execute()
+const app = Express()
+app.get("/produtos",async(req,res)=>{
+    const listaProdutos = new ListaProdutos()
+    res.send(await listaProdutos.execute())
+})
+
+let porta = 8000
+app.listen(porta,()=>{
+    console.log("Server Rodando")
+    console.log("digite: localhost:8000/produtos na url para acessar o servidor.")
+    
+});
+

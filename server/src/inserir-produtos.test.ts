9a67261af93bec5c794c7dfbee51c8bda8b00cf3
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2024-08-31 09:54:25 -0400

    Código Inserir produto

--- server/src/inserir-produtos.test.ts
+++ server/src/inserir-produtos.test.ts
@@ -1,6 +1,8 @@
 import { expect , test , beforeEach } from 'vitest'
 import mysql from 'mysql2/promise'
 import 'dotenv/config'
+import ListaProdutos from './lista-produtos';
+import InserirProdutos from './inserir-produtos'
 beforeEach(async()=>{
     const connection = await mysql.createConnection({
         host: process.env.DB_HOST,
@@ -11,6 +13,22 @@ beforeEach(async()=>{
     await connection.query("insert into produtos values(?,?,?,?,?)",
         ['1','Iphone','Celular Ruim','4000.50','https://s2-techtudo.glbimg.com/fQiJ0IoTPyS7kOji53qOHDP_VWM=/0x0:4000x2664/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/J/F/1YRriISlAbBwU7zUr7wQ/marca-d-aguadd.png'])
 })
-test("Deve inserir um produto no banco de dados",()=>{
+test("Deve inserir um produto no banco de dados",async()=>{
+    //GIVEN
+    const produtoParaInserir = {
+        id:2,
+        nome:"Samsung",
+        descricao:"Celular da Samsung",
+        preco: 2000,
+        imagem: "SEM IMAGEM"
+    }
+    //WHEN
+    const inserirProdutos = new InserirProdutos()
+    const produtoBanco = await inserirProdutos.execute(produtoParaInserir)
+    //THEN
+    expect(produtoBanco).toStrictEqual(produtoParaInserir)
+    const listaProdutos = new ListaProdutos()
+    const produtos = await listaProdutos.execute()
+    expect(produtos).toContain(produtoParaInserir)
 
 })
---
+++ server/src/inserir-produtos.ts
@@ -0,0 +1,29 @@
+import mysql from 'mysql2/promise'
+import 'dotenv/config'
+export default class InserirProdutos{
+    async execute(input:Input):Promise<Output>{
+        const connection = await mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USUARIO,
+            database: process.env.DB_BANCO,
+        });
+        await connection.query("insert into produtos values(?,?,?,?,?)",
+        [])
+
+        return input
+    }
+}
+type Input = {
+    id:number,
+    nome:string,
+    descricao:string,
+    preco:number,
+    imagem:string
+}
+type Output = {
+    id:number,
+    nome:string,
+    descricao:string,
+    preco:number,
+    imagem:string
+}

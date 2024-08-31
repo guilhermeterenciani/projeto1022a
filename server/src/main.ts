4f06ca8422844cd37e02167b6a6a33f19ffe3924
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2024-08-31 08:00:25 -0400

    Listando dados da API no WEB

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,16 +1,17 @@
 import Express from "express";
+import Cors from 'cors'
 import ListaProdutos from "./lista-produtos";
 
 const app = Express()
+app.use(Cors())
 app.get("/produtos",async(req,res)=>{
     const listaProdutos = new ListaProdutos()
     res.send(await listaProdutos.execute())
 })
 
-let porta = 8000
+const porta = 8000
 app.listen(porta,()=>{
     console.log("Server Rodando")
     console.log("digite: localhost:8000/produtos na url para acessar o servidor.")
-    
-});
+})
 

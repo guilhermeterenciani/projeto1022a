98f2f487dbecf0cb345d31a22033b241a187aabc
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2024-09-02 07:21:05 -0400

    Código de inserir produto no banco funcionando.

--- server/src/inserir-produtos.test.ts
+++ server/src/inserir-produtos.test.ts
@@ -10,8 +10,6 @@ beforeAll(async()=>{
         database: process.env.DB_BANCO,
     });
     await connection.query("DELETE FROM produtos");
-    await connection.query("insert into produtos values(?,?,?,?,?)",
-        ['1','Iphone','Celular Ruim','4000.50','https://s2-techtudo.glbimg.com/fQiJ0IoTPyS7kOji53qOHDP_VWM=/0x0:4000x2664/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/J/F/1YRriISlAbBwU7zUr7wQ/marca-d-aguadd.png'])
 })
 afterAll(async()=>{
     const connection = await mysql.createConnection({
@@ -37,6 +35,6 @@ test("Deve inserir um produto no banco de dados",async()=>{
     expect(produtoBanco).toStrictEqual(produtoParaInserir)
     const listaProdutos = new ListaProdutos()
     const produtos = await listaProdutos.execute()
-    expect(produtos).toContain(produtoParaInserir)
+    expect(produtos).toContainEqual(produtoParaInserir)
 
 })

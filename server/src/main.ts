b024eff4bb66f2dfcb6ae73c41ef50d41e21801e
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2024-09-02 08:21:09 -0400

    Código de Cadastrar Produto

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,13 @@
 import Express from "express";
 import Cors from 'cors'
 import ListaProdutos from "./lista-produtos";
+import InserirProdutos from "./inserir-produtos";
+
 
 const app = Express()
 app.use(Cors())
+app.use(Express.json())
+
 app.get("/produtos",async(req,res)=>{
     const listaProdutos = new ListaProdutos()
     res.send(await listaProdutos.execute())
@@ -11,6 +15,18 @@ app.get("/produtos",async(req,res)=>{
 
 app.post("/produtos",async(req,res)=>{
     console.log("Alguém tentou cadastrar Produtos")
+    const {id,nome,descricao,preco,imagem} = req.body
+    const produto = {
+        id,
+        nome,
+        descricao,
+        preco,
+        imagem
+    }
+    const inserirProduto = new InserirProdutos()
+    const produtoInserido = inserirProduto.execute(produto)
+    res.status(201).send(produtoInserido)
+
 })
 
 const porta = 8000
